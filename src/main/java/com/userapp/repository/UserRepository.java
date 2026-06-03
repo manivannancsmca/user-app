70a91e7bf9789cb29c67016d2be6e33717740da0
manivannancsmca <manivannancsmca@gmail.com>
2026-06-03 23:41:53 +0530

    Update UserRepository.java

--- src/main/java/com/userapp/repository/UserRepository.java
+++ src/main/java/com/userapp/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.userapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.userapp.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
